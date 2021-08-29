85dfba02ffc02dbf21c4f1f861611714f2370db5
Konstantin Arakantsev <kostya3312@gmail.com>
2021-08-29 10:38:44 +0300

    use dotenv

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const winston = require("winston");
-const express = require("express");
-const config = require("config");
-const app = express();
+require("dotenv").config()
 
-require("./startup/logging")();
-require("./startup/cors")(app);
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
-require("./startup/validation")();
+const winston = require("winston")
+const express = require("express")
+const config = require("config")
+const app = express()
 
-const port = process.env.PORT || config.get("port");
+require("./startup/logging")()
+require("./startup/cors")(app)
+require("./startup/routes")(app)
+require("./startup/db")()
+require("./startup/config")()
+require("./startup/validation")()
+
+const port = process.env.PORT || config.get("port")
 const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
-);
+    winston.info(`Listening on port ${port}...`)
+)
 
-module.exports = server;
+module.exports = server
