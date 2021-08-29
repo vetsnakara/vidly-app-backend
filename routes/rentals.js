6ad3749b545e35a281dd06f734d93d64ea1acc7c
Konstantin Arakantsev <kostya3312@gmail.com>
2021-08-29 10:17:17 +0300

    actualization

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,72 +1,72 @@
-const { Rental, validate } = require("../models/rental");
-const { Movie } = require("../models/movie");
-const { Customer } = require("../models/customer");
-const auth = require("../middleware/auth");
-const mongoose = require("mongoose");
-const Fawn = require("fawn");
-const express = require("express");
-const router = express.Router();
-
-Fawn.init(mongoose);
+const { Rental, validate } = require("../models/rental")
+const { Movie } = require("../models/movie")
+const { Customer } = require("../models/customer")
+const auth = require("../middleware/auth")
+const mongoose = require("mongoose")
+const express = require("express")
+const router = express.Router()
 
 router.get("/", auth, async (req, res) => {
-  const rentals = await Rental.find()
-    .select("-__v")
-    .sort("-dateOut");
-  res.send(rentals);
-});
+    const rentals = await Rental.find().select("-__v").sort("-dateOut")
+    res.send(rentals)
+})
 
 router.post("/", auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+    const { error } = validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
 
-  const customer = await Customer.findById(req.body.customerId);
-  if (!customer) return res.status(400).send("Invalid customer.");
+    const customer = await Customer.findById(req.body.customerId)
+    if (!customer) return res.status(400).send("Invalid customer.")
 
-  const movie = await Movie.findById(req.body.movieId);
-  if (!movie) return res.status(400).send("Invalid movie.");
+    const movie = await Movie.findById(req.body.movieId)
+    if (!movie) return res.status(400).send("Invalid movie.")
 
-  if (movie.numberInStock === 0)
-    return res.status(400).send("Movie not in stock.");
+    if (movie.numberInStock === 0)
+        return res.status(400).send("Movie not in stock.")
 
-  let rental = new Rental({
-    customer: {
-      _id: customer._id,
-      name: customer.name,
-      phone: customer.phone
-    },
-    movie: {
-      _id: movie._id,
-      title: movie.title,
-      dailyRentalRate: movie.dailyRentalRate
+    let rental = new Rental({
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            phone: customer.phone,
+        },
+        movie: {
+            _id: movie._id,
+            title: movie.title,
+            dailyRentalRate: movie.dailyRentalRate,
+        },
+    })
+
+    try {
+        const session = await mongoose.connection.startSession()
+
+        await Rental.create(rental, { session })
+
+        await Movie.updateOne(
+            { _id: movie._id },
+            {
+                $inc: { numberInStock: -1 },
+            },
+            { session }
+        )
+
+        session.endSession()
+
+        res.send(rental)
+    } catch (ex) {
+        res.status(500).send("Something failed.")
     }
-  });
-
-  try {
-    new Fawn.Task()
-      .save("rentals", rental)
-      .update(
-        "movies",
-        { _id: movie._id },
-        {
-          $inc: { numberInStock: -1 }
-        }
-      )
-      .run();
-
-    res.send(rental);
-  } catch (ex) {
-    res.status(500).send("Something failed.");
-  }
-});
+})
 
 router.get("/:id", [auth], async (req, res) => {
-  const rental = await Rental.findById(req.params.id).select("-__v");
+    const rental = await Rental.findById(req.params.id).select("-__v")
 
-  if (!rental)
-    return res.status(404).send("The rental with the given ID was not found.");
+    if (!rental)
+        return res
+            .status(404)
+            .send("The rental with the given ID was not found.")
 
-  res.send(rental);
-});
+    res.send(rental)
+})
 
-module.exports = router;
+module.exports = router
--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,15 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
-const config = require('config');
+const winston = require("winston")
+const mongoose = require("mongoose")
+const config = require("config")
 
-module.exports = function() {
-  const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
+module.exports = function () {
+    const db = config.get("db")
+
+    mongoose
+        .connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => winston.info(`Connected to ${db}...`))
+        .catch(console.error)
 }
